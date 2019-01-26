a27fcacc3a042fcfc3558c14b7376d990b6b9adb
Marc <magueracoras@gmail.com>
2019-01-26 22:01:33 +0100

    Day One

--- The Call/Assets/Scripts/EventController.cs
+++ The Call/Assets/Scripts/EventController.cs
@@ -1,20 +1,113 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using TMPro;
+
+[Serializable]
+public class EventDialogue
+{
+    public enum EventType { dialogue, decision, endgame }
+    public EventType eventType;
+
+    public string dialogue;
+
+    public GameObject decisionPanel;
+}
 
 public class EventController : MonoBehaviour
 {
+    public List<EventDialogue> eventList;
+
+    private bool startEvent;
+    private bool isFinished;
+
     [SerializeField]
-    private string myEvent;
-    // Start is called before the first frame update
-    void Start()
+    private float scrollSpeed;
+
+    [SerializeField]
+    private int index;
+    private int currentEvent;
+    private bool showingDialogue;
+
+    private int times;
+
+    public int Index
     {
-        
+        get
+        {
+            return index;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private MainController controller;
+
+    [SerializeField]
+    private TextMeshProUGUI textTM;
+
+    Coroutine textCoroutine;
+
+    private void Start()
     {
-        
+        controller = FindObjectOfType<MainController>();
+    }
+
+    public void NextDialogue()
+    {
+        times++;
+        if (currentEvent >= eventList.Count - 1)
+            return;
+
+        if (!showingDialogue)
+        {
+            textCoroutine = StartCoroutine(Scrolltext(eventList[currentEvent].dialogue));
+            isFinished = false;
+            
+        }
+        else
+        {
+            if (!isFinished)
+            {
+
+                textTM.text = eventList[currentEvent].dialogue;
+                isFinished = true;
+                StopCoroutine(textCoroutine);
+            }
+            else
+            {
+                currentEvent++;
+                if (eventList[currentEvent].eventType == EventDialogue.EventType.dialogue)
+                {
+                    showingDialogue = false;
+                    NextDialogue();
+                    
+                }
+                else if (eventList[currentEvent].eventType == EventDialogue.EventType.decision)
+                {
+                    eventList[currentEvent].decisionPanel.SetActive(true);
+                }
+                else
+                {
+                    controller.GameOver();
+                }
+            }
+        }
+
+    }
+
+    IEnumerator Scrolltext(string text)
+    {
+        for (int i = 0; i <= text.Length; i++)
+        {
+            textTM.text = text.Substring(0, i);
+            if (i == text.Length)
+            {
+                //if(canSkip) StartCoroutine(skipMessageCoroutine);
+                isFinished = true;
+            }
+            yield return new WaitForSeconds(scrollSpeed);
+            showingDialogue = true;
+            //writingText = "";            
+        }
     }
 }
--- The Call/Assets/Scripts/MainController.cs
+++ The Call/Assets/Scripts/MainController.cs
@@ -1,22 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class MainController : MonoBehaviour
 {
-    public delegate void OnShowEvent();
+    [SerializeField]
+    private EventController[] eventControllers;
+    UnityAction action;
+    [SerializeField]
+    private Button button;
+    private int prevEvent;
 
-    public OnShowEvent ShowEvent { get; set; }
+    [SerializeField]
+    private GameObject gameOverPanel;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private int maxResistance;
+    public int currentResistance;
+
+    [SerializeField]
+    private Image resistanceBar;
+
+    private EventController currentEvent;
+
+
+    private void Start()
     {
+        StartEvent(0);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextEvent()
     {
-        
+        currentEvent.NextDialogue();
+    }
+
+    public void StartEvent(int index)
+    {
+        for (int i = 0; i < eventControllers.Length; i++)
+        {
+            if (eventControllers[i].Index == index)
+            {
+                currentEvent = eventControllers[i];
+                break;
+            }
+
+        }
+
+        NextEvent();
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void AddResistance(int points)
+    {
+        currentResistance += points;
+
+        resistanceBar.fillAmount = (float)currentResistance / (float)maxResistance;
+        Debug.Log(resistanceBar.fillAmount);
     }
 }

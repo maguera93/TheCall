8585a9335858107330f27f2974d3b29ffa32c23a
Marc <magueracoras@gmail.com>
2019-01-26 10:24:37 +0100

    getting started

---
+++ The Call/Assets/Scripts/EventController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventController : MonoBehaviour
+{
+    [SerializeField]
+    private string myEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ The Call/Assets/Scripts/MainController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    public delegate void OnShowEvent();
+
+    public OnShowEvent ShowEvent { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
